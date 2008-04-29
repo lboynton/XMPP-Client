35df194d0f0851de26e8fc65b5f451796fb7013b
lboynton <lee@lboynton.com>
2008-04-29 17:24:44 +0000

    Added icons class
    git-svn-id: svn+ssh://192.168.0.8/svn/B382/trunk@37 b4e1a2f0-0047-11dd-a6bb-41aab4003810

---
+++ src/xmppclient/images/Icons.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package xmppclient.images;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Lee
+ */
+public class Icons
+{
+    public static final ImageIcon busy = new javax.swing.ImageIcon(Icons.class.getResource("status_busy.png"), "Busy status icon");
+    public static final ImageIcon offline = new javax.swing.ImageIcon(Icons.class.getResource("status_offline.png"), "Offline status icon");
+    public static final ImageIcon away = new javax.swing.ImageIcon(Icons.class.getResource("status_away.png"), "Away status icon");
+    public static final ImageIcon online = new javax.swing.ImageIcon(Icons.class.getResource("status_online.png"), "Online status icon");
+    public static final ImageIcon error = new javax.swing.ImageIcon(Icons.class.getResource("error.png"), "Error icon");
+}
